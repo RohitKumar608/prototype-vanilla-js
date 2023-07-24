51cd0621a65d8c69790fd4f4dabf385d6ea45d3a
rohitkumarm <rohitkumarrk13568@gmail.com>
2023-07-24 11:13:12 +0530

    feat: Object.create replaced with Object.setPrototypeOf

--- subClassWithNew.js
+++ subClassWithNew.js
@@ -20,11 +20,21 @@ function paidUserCreator(name, score, accountBalance) {
   this.accountBalance = accountBalance
 }
 
-paidUserCreator.prototype = Object.create(userCreator.prototype)
-paidUserCreator.prototype.incrementBalance = function () {
+/*  if we are using the Object.create and assigning to paidUserCreator.prototype i will override the previously prototype assigned to paidUserCreator
+ because it will create a new object and replace the previous object with new one
+*/
+// paidUserCreator.prototype = Object.create(userCreator.prototype)
+
+/*  if we are using the Object.setPrototypeOf and setting the prototype i will add the new prototype to paidUserCreator.prototype
+ */
+Object.setPrototypeOf(paidUserCreator.prototype, userCreator.prototype)
+
+paidUserCreator.prototype.increaseBalance = function () {
   this.accountBalance++
 }
 
 const paidUser1 = new paidUserCreator('David', 5, 50)
+paidUser1.increaseBalance()
+paidUser1.sayName()
 
 console.log(paidUser1)
